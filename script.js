efd839eb9c753b8f2cdfd1fc758157781ad08cc1
selajdin89 <selajdinmemeti89@gmail.com>
2022-10-16 23:06:32 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+import { readFileSync } from "fs";
+
+function syncReadFile(filename) {
+  const contents = readFileSync("./dictionary.txt", "utf-8");
+
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+
+const words = syncReadFile("./dictionary.txt");
+
+const sort = (word) => word.split("").sort().join("");
+
+const anagramsFinder = (word, words) => {
+  let sortedWord = sort(word);
+  let totalAnagrams = [];
+
+  for (let n = 0; n < words.length; n++) {
+    if (sort(words[n]) === sortedWord && words[n] !== word) {
+      totalAnagrams.push(words[n]);
+    }
+  }
+  return totalAnagrams.join(", ");
+};
+
+console.log(anagramsFinder("task", words));
